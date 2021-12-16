f5d3768268843a82dd37c8ec484fb707f31325fb
kidslabkids <kidslab@KidsLabStation>
2021-12-16 15:52:45 +0100

    kettensäge

---
+++ src/main/java/net/mcreator/matt/item/A1Item.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.matt.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.matt.MattModElements;
+
+@MattModElements.ModElement.Tag
+public class A1Item extends MattModElements.ModElement {
+	@ObjectHolder("matt:a_1")
+	public static final Item block = null;
+	public A1Item(MattModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 13f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, 6f, new Item.Properties().group(ItemGroup.COMBAT)) {
+		}.setRegistryName("a_1"));
+	}
+}
